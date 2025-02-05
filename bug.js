86327a83f522a2f5e5c3807483f7e4287f03ccbb
BugHunter-X <askzenai@gmail.com>
2025-02-05 14:36:12 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling for invalid userId
+  db.getUser(userId, (err, user) => {
+    if (err) {
+      // Sending a generic error message is bad practice. 
+      // Should provide more details for debugging
+      res.status(500).send('Error');
+    } else {
+      res.json(user);
+    }
+  });
+});
